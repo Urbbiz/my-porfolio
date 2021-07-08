409714d3ee4f2b262eb235e9a1bf2fd52e8c5d47
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-07-08 09:44:46 +0300

    commit

--- img/work/php.php
+++ img/work/php.php
@@ -1,21 +1,64 @@
-?php
+$givenWord = ".".$givenWord.".";  // uzdedam taskus
 
-class ExtractionValues {
-    
-  
-
-    // isspausdina values is txt filo
-    // reikia gautus duomenis isirasyti i masyva runtime metu.
-    public static function getValues(){
-        $values = [];
-        $file = new SplFileObject("inputfile.txt");
-    while (!$file->eof()) {
-    $values[] = $file->fgets();
-    // echo $file->fgets();
-    // echo "\n","\n";
+// <--------perdarom i masiva be skaiciu-------->
+$valuesNoNumbers= [];
+foreach ($values as $value){
+$valuesNoNumbers[] = str_replace(['1','2','3','4','5','6','7','8','9','0'], "",$value );
 }
-// echo count($values);
-// echo print_r($values);
-return $values;
+
+// echo  $valuesNoNumbers[0];
+
+
+$foundValues= [];
+foreach ($valuesNoNumbers as $key=> $value){
+// echo $key."-". $value ;
+// print_r($value);
+$found=false;
+do {
+// echo $key;
+$offset = 0;
+if ($found !== false) {
+    $offset = $found + 1;
+    $snippet = $values[$key];
+    
+    $snippetIndex = 0; 
+    for($i= 0; $i < strlen($snippet); $i++ ){
+        
+        $number = intval($snippet[$i]);
+        if($number > 0 ){
+
+          $index = $snippetIndex + $found -1;
+            if (!array_key_exists($index, $foundValues) || $foundValues[$index] < $number ){
+                 $foundValues[$index] = $number; 
+            }
+        }else {
+             $snippetIndex ++;   // didins kai tik atras raide, o ne skaiciu 
+        }
     }
+    
+    // echo "positionInWord: ". $found .", value: " . $values[$key] . "\n";
+}
+$found = stripos($givenWord, $value, $offset);
+
+}while($found !== false);
+
+
+
+//print_r($valuesNoNumbers[4446]); 
+}
+
+$finalResult = "";
+for ($i=0; $i < strlen($givenWord) ; $i++) { 
+$finalResult .=  $givenWord[$i];
+
+if(array_key_exists($i,$foundValues ) && $foundValues[$i]% 2 ==1 ){
+$finalResult .=  "-";
+
+
+// echo $foundValues[$i];
+}
+}
+return trim($finalResult, ".");
+
+
 }
